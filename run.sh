75a06404bf0f734eb6aa7be8c34b29b41e20ddfe
elanezon <elanezon@gmail.com>
2023-09-27 00:43:17 -0600

    added .sh and video

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python gesture_recognition_demo.py \
+python3 gesture_recognition_demo.py \
     -m_a /home/root/OpenVinoZooDemo/intel/asl-recognition-0004/FP16/asl-recognition-0004.xml \
     -m_d /home/root/OpenVinoZooDemo/intel/person-detection-asl-0001/FP16/person-detection-asl-0001.xml \
     -i /home/root/OpenVinoZooDemo/BasicASLConversation.mp4 \
