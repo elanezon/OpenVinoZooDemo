4a6767c4df57d1da047e33d488a3391697845b0f
elanezon <elanezon@gmail.com>
2023-09-28 18:06:26 -0600

    added source to run script

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+source openvino_env/bin/activate
 python3 gesture_recognition_demo.py \
     -m_a /home/root/OpenVinoZooDemo/intel/asl-recognition-0004/FP16/asl-recognition-0004.xml \
     -m_d /home/root/OpenVinoZooDemo/intel/person-detection-asl-0001/FP16/person-detection-asl-0001.xml \
